133b8501e46a4d021699dafd277749f6e77081f9
田原 一樹 <ssp0727.lnc@gmail.com>
2019-10-05 22:21:06 +0900

    [update]

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { increment, decrement } from '../actions'
+
+class App extends Component {
+  render () {
+    const props = this.props
+    console.info('props', this.props)
+    console.info('props', props)
+
+    return (
+      <>
+        <div>value:{ props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </>
+    )
+  }
+ }
+
+ const mapStateToProps = state => ({ value: state.counts.value })
+
+ const mapDispatchToProps = ({ increment, decrement })
+
+ export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux'
+
+import counts from './count'
+
+export default combineReducers({ counts })
