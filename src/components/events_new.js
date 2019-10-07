42565529abdb4c2b929ffccc7c2a2d31620267eb
田原 一樹 <ssp0727.lnc@gmail.com>
2019-10-07 22:47:26 +0900

    [update]

---
+++ src/components/events_new.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+
+import { postEvent } from '../actions'
+
+class EventsNew extends Component {
+  
+}
